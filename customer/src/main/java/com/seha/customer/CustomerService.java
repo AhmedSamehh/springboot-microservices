76160543d2c83bf721b55cc1d4b13cf0ffbee145
AhmedSamehh <a7medse7a10@gmail.com>
2023-11-07 17:52:09 +0200

    connecting to db

--- customer/src/main/java/com/seha/customer/CustomerService.java
+++ customer/src/main/java/com/seha/customer/CustomerService.java
@@ -3,12 +3,13 @@ package com.seha.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void RegisterCustomer(CustomerRegisterationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
                 .lastName(request.lastName())
                 .email(request.email())
                 .build();
+        customerRepository.save(customer);
     }
 }
